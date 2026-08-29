81c3d6ca3c838c54b70d354192dd15da0e622324
shivani <moonlovers.abc@gmail.com>
2026-08-29 15:49:41 +0530

    CORS issue solved

--- server/app.js
+++ server/app.js
@@ -1,13 +1,20 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+
 import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
+
 app.use(express.json());
 
-app.get("/" ,(req,res) => {
+app.get("/", (req, res) => {
     res.send("hello from app.js");
 });
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
